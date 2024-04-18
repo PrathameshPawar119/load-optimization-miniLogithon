f2b4e91b29c363a8e8587c74a0a377de6d7f0893
PrathameshPawar119 <cm.b26.prathameshpawar@gmail.com>
2024-04-18 22:48:14 +0530

    added image save function

--- container-app/app.py
+++ container-app/app.py
@@ -1,9 +1,10 @@
 from viktor.core import ViktorController
 from viktor.geometry import SquareBeam, Group, Material
-from viktor.parametrization import ViktorParametrization, Text, IntegerField, OptionField, DynamicArray
+from viktor.parametrization import ViktorParametrization, Text, IntegerField, OptionField, DynamicArray, ActionButton
 from viktor.views import SVGView, SVGResult, GeometryResult, GeometryView
 from viktor import Color
 from io import StringIO
+import os
 from matplotlib.patches import Rectangle
 
 from solver import solver
@@ -13,6 +14,7 @@ from rectpack import newPacker
 import rectpack.packer as packer
 import matplotlib.pyplot as plt
 import random
+from datetime import datetime
 
 
 class Parametrization(ViktorParametrization):
@@ -30,10 +32,108 @@ class Parametrization(ViktorParametrization):
                                    default='120 x 80 cm: Standard Euro Pallet')
     array.pallet_quantity = IntegerField('Quantity', default=1, flex=25)
 
+    Finalize_Button = ActionButton('Finalize Container 🚀', method='save_image')
+
+
 
 class Controller(ViktorController):
     label = 'My Container'
     parametrization = Parametrization(width=35)
+
+    def save_image(self, params, **kwargs):
+        # Create the folder if it doesn't exist
+        '''This function creates the output view plotting the pallets and container.'''
+
+        # initialize figure
+        fig = plt.figure(figsize=(4, 12))
+
+        # Set Containers size
+        bins20 = [(235, 590)]  # 20' container
+        bins40 = [(235, 1203)]  # 40' container
+        # https://www.dsv.com/fr-fr/nos-solutions/modes-de-transport/fret-maritime/dimensions-de-conteneurs-maritimes/dry-container
+        # https://www.icontainers.com/help/how-many-pallets-fit-in-a-container/
+
+        # Set Pallet Dimensions
+        bx = 5  # buffer x
+        by = 5  # buffer y
+
+        pal_128 = [120 + bx, 80 + by]
+        pal_68 = [60 + bx, 80 + by]
+        pal_64 = [60 + bx, 40 + by]
+        pal_1210 = [120 + bx, 100 + by]
+        pal_610 = [60 + bx, 100 + by]
+        pal_65 = [60 + bx, 50 + by]
+
+        # How many of each pallet type is selected
+        n_128, n_68, n_64, n_1210, n_610, n_65 = 0, 0, 0, 0, 0, 0
+
+        for pallet in params.array:
+            if pallet.pallet_dim == '120 x 80 cm: Standard Euro Pallet':
+                n_128 += pallet.pallet_quantity
+            if pallet.pallet_dim == '60 x 80 cm: Half Euro Pallet':
+                n_68 += pallet.pallet_quantity
+            if pallet.pallet_dim == '60 x 40 cm: Display Pallet':
+                n_64 += pallet.pallet_quantity
+            if pallet.pallet_dim == '120 x 100 cm: Standard Block Pallet':
+                n_1210 += pallet.pallet_quantity
+            if pallet.pallet_dim == '60 x 100 cm: Half Block Pallet':
+                n_610 += pallet.pallet_quantity
+            if pallet.pallet_dim == '60 x 50 cm: HST mini-pallet':
+                n_65 += pallet.pallet_quantity
+
+        # Plot Container
+        if params.bin_type == "20'":
+            bin_type = bins20
+            plt.plot([0, 235, 235, 0, 0], [0, 0, 590, 590, 0], linewidth=2.5, color="k")
+        else:
+            bin_type = bins40
+            plt.plot([0, 235, 235, 0, 0], [0, 0, 1203, 1203, 0], linewidth=2.5, color="k")
+
+        # all_rects, all_pals = solver(params.n_812, params.n_1012, bin_type)
+        all_rects, all_pals = solver(n_128, n_68, n_64, n_1210, n_610, n_65, bin_type)
+
+        # Loop all rect
+        for rect in all_rects:
+            b, x, y, w, h, rid = rect
+            # Pallet type colours. If included also add color in plot below.
+            if [w, h] == pal_128 or [h, w] == pal_128:
+                color = 'pink'
+            if [w, h] == pal_68 or [h, w] == pal_68:
+                color = 'brown'
+            if [w, h] == pal_64 or [h, w] == pal_64:
+                color = 'olive'
+            if [w, h] == pal_1210 or [h, w] == pal_1210:
+                color = 'orange'
+            if [w, h] == pal_610 or [h, w] == pal_610:
+                color = 'blue'
+            if [w, h] == pal_65 or [h, w] == pal_65:
+                color = 'purple'
+            plt.gca().add_patch(Rectangle((x, y), w, h, facecolor=color, edgecolor='k', fill=True, lw=2))
+
+        plt.axis('equal')
+        plt.axis('off')
+        fig.tight_layout()
+
+        # save figure
+        svg_data = StringIO()
+        fig.savefig(svg_data, format='svg')
+        plt.close()
+        # Create the folder if it doesn't exist
+        if not os.path.exists('images'):
+            os.makedirs('images')
+
+        # Create the file path
+        current_time = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        filename = f"svg_result_{current_time}.svg"
+        file_path = os.path.join('images', filename)
+
+        # Write the SVG data to a file
+        with open(file_path, 'w') as f:
+            f.write(svg_data.getvalue())
+
+        print(f"SVG image saved to: {file_path}")
+        print(f"Params : {params}")
+        ...
 
     @SVGView("container", duration_guess=1)
     def create_svg_result(self, params, **kwargs):
@@ -118,7 +218,7 @@ class Controller(ViktorController):
 
     @GeometryView("3D container", duration_guess=1)
     def visualize_container(self, params, **kwargs):
-
+        print(params)
         # generate container
         length_x = 2.35
         length_z = 2.6
